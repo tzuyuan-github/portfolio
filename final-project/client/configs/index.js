2e130c95a7994385c5b652518160900d21dbed0e
tzu <145038407+tzuyuan-github@users.noreply.github.com>
2024-01-26 00:42:37 +0800

    Delete final-project directory

--- final-project/client/configs/index.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 6005
-export const DEV = true
-
-// express 的位置
-export const apiBaseUrl = 'http://localhost:3005/api'
-export const avatarBaseUrl = 'http://localhost:3005/avatar'
