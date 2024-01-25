d5f30750ffed6c3b69a722dd1d57994fa0e8686f
tzu <145038407+tzuyuan-github@users.noreply.github.com>
2024-01-26 00:42:47 +0800

    Delete midterm-project directory

--- midterm-project/address-book/parts/scripts.php
+++
@@ -1,2 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
--- midterm-project/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-# 清除所有 session 資料
-unset($_SESSION['admin']);
-session_destroy();
-
-header('Location: login.php');
